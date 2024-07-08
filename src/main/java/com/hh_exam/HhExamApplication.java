26d50d5b95ed87f56364f913f5380fc7f1065cf1
hym <ghrltjdtprbs@gmail.com>
2024-07-08 10:19:44 +0900

    [feat] 프로젝트 초기 세팅
    변경 사항 :
     - 프로젝트 시작을 위한 초기 세팅

---
+++ src/main/java/com/hh_exam/HhExamApplication.java
@@ -0,0 +1,13 @@
+package com.hh_exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhExamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hh_exam/HhExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hh_exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
