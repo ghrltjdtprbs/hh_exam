89f7c62c5b07b2cd976db4ad5c945429e9adf8de
hym <ghrltjdtprbs@gmail.com>
2024-07-08 10:34:04 +0900

    [feat] Item 엔터티 추가
    변경 사항 :
     - 요구사항에 맞게 Item 엔터티를 추가하였습니다.

---
+++ src/main/java/entity/Item.java
@@ -0,0 +1,33 @@
+package entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@Table(name = "Item")
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    private int price;
+
+    private String username;
+}
